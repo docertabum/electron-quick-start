4ff7ae35fb4f8edab82752b89ed12cdeda577c6b
krn <szm.bordel@gmail.com>
2019-10-28 22:04:15 +0100

    demo: expanse of index.html content etc

--- preload.js
+++ preload.js
@@ -1,12 +1,35 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  } 
-  
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+    console.log(window);
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector);
+        if (element) element.innerText = text
+    };
+
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("done!"), 1000);
+    });
+
+    // resolve runs the first function in .then
+    promise.then(
+        result => document.getElementById("pElement1").innerText = result+1,
+        error => alert(error) // doesn't run
+    );
+
+    for (const type of ['chrome', 'node', 'electron']) {
+        replaceText(`${type}-version`, process.versions[type])
+    }
+    console.log("daco");
+    const btnTest = document.getElementById("btnTest");
+    console.log(btnTest);
+    btnTest.addEventListener("click", function () {
+            alert("daco");
+            document.getElementById("pElement1").innerText = "boo";
+            const {remote} = require('electron')
+            remote.getCurrentWindow().loadFile('./src/indexedDBdemo.html');
+        }
+    );
+});
+
+
